62434a1e139fe9e245a182d5a5b88bd70e8bea92
ttt陶婷婷(IT) <tingtingtao@ctrip.com>
2018-12-17 11:30:44 +0800

    jquery jsonp

--- myweb/public/javascripts/index.js
+++ myweb/public/javascripts/index.js
@@ -43,3 +43,13 @@ document.getElementById('btn-4').addEventListener('click', function() {
     document.getElementsByTagName('head')[0].appendChild(script);
 });
 
+document.getElementById('btn-5').addEventListener('click', function() {
+    $.ajax({
+        url: 'http://127.0.0.1:3000/info/jsonp?cb=myCallback',
+        dataType: 'jsonp', // 注意，此处dataType的值表示请求使用JSONP
+        jsonp: 'cb', // 请求query中callback函数的键名
+    }).done(function (res) {
+        alert(JSON.stringify(res, null , 2));
+    });
+    
+});
